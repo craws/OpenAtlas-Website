0494b2f94b6c8ab5de5ad17a565cfb24b20bf6eb
Alexander Watzinger <alexander.watzinger@craws.net>
2019-04-30 15:00:38 +0200

    Added standard error templates and credits

--- openatlas_website/views/index.py
+++ openatlas_website/views/index.py
@@ -7,3 +7,23 @@ from openatlas_website import app
 @app.route('/')
 def index():
     return render_template('index.html', intro='This is the intro')
+
+
+@app.route('/credits')
+def index_credits():
+    return render_template('credits.html')
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html', e=e), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', e=e), 404
+
+
+@app.errorhandler(418)
+def invalid_id(e):
+    return render_template('418.html', e=e), 418
