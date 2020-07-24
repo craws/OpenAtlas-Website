9654b0e8d7c3944d0264dba88d18c584d743f3aa
Alexander Watzinger <alexander.watzinger@craws.net>
2020-07-24 08:49:05 +0200

    Update events

--- openatlas_website/views/events.py
+++ openatlas_website/views/events.py
@@ -7,13 +7,14 @@ from openatlas_website import app
 
 @app.route('/events')
 def events() -> str:
-    upcoming = OrderedDict([('2020-07-23', {'country': 'Austria',
-                                            'city': 'Vienna',
-                                            'name': 'OpenAtlas Summer Meeting'}),
+    upcoming = OrderedDict([
 
                             ])
     past = OrderedDict([
         ('2020', OrderedDict([
+            ('2020-07-23', {'country': 'Austria',
+                            'city': 'Vienna',
+                            'name': 'OpenAtlas Summer Meeting'}),
             ('2020-06-17', {'country': 'Austria',
                             'city': 'Vienna',
                             'institute': 'Vienna Museum of Natural History',
