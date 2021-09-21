1fe2d11dfcf3cc078f4bf788570569af8088eb4f
Alexander Watzinger <alexander.watzinger@craws.net>
2021-09-21 14:50:19 +0200

    Tying GitHub test action

--- config/default.py
+++ config/default.py
@@ -1,4 +1,5 @@
 # Don't edit this file. To override settings please use instance/production.py
+from flask import Config
 
 VERSION = '0.1.0'
 LANGUAGES = {'en': 'English', 'de': 'Deutsch'}
@@ -6,6 +7,11 @@ DEBUG = False
 SECRET_KEY = 'CHANGE ME'
 
 # Security
-SESSION_COOKIE_SECURE = False  # Should be set to True in production.py if using HTTPS only
+SESSION_COOKIE_SECURE = False  # Should be True in production.py if using HTTPS
 REMEMBER_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = 'Lax'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SECRET_KEY = "AAaajjjfk887856$%kk"
