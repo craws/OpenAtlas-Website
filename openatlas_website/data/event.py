5a13f34846d6ced956c3d7fe451118b35c5d5772
Alexander Watzinger <alexander.watzinger@craws.net>
2022-10-15 14:40:49 +0200

    Update events

--- openatlas_website/data/event.py
+++ openatlas_website/data/event.py
@@ -1,11 +1,5 @@
 
 upcoming = {
-    '2022-10-14': {
-        'country': 'Austria',
-        'city': 'Vienna',
-        'institute': 'TU Wien',
-        'name': 'INDIGO Status quo meeting',
-        'title': 'Status quo - New OpenAtlas features for INDIGO'},
     '2022-10-25': {
         'country': 'Austria',
         'city': 'Vienna',
@@ -14,7 +8,7 @@ upcoming = {
         'name': 'ACDH-CH Research lunch',
         'title':
             'OpenAtlas - A Database System for the Humanities and Beyond'},
-    '2023-06-21': {
+    '2023-06-14': {
         'country': 'Austria',
         'city': 'Vienna',
         'institute': 'Austrian Academy of Sciences',
@@ -40,6 +34,12 @@ upcoming = {
 
 past = {
     '2022': {
+        '2022-10-14': {
+            'country': 'Austria',
+            'city': 'Vienna',
+            'institute': 'TU Wien',
+            'name': 'INDIGO Status quo meeting',
+            'title': 'Status quo - New OpenAtlas features for INDIGO'},
         '2022-10-12': {
             'country': 'Germany',
             'city': 'Mainz',
