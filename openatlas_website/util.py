0a2a30307573bcf533b3721826f23d0a825b4f0a
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2023-03-22 14:29:30 +0100

    Workshop Changes: Nav Improvements

--- openatlas_website/util.py
+++ openatlas_website/util.py
@@ -15,22 +15,29 @@ def inject_menu() -> dict[str, Any]:
     menu = [
         {'name': 'about', 'to': url_for('about')},
         {'name': 'projects', 'to': url_for('projects')},
+        {'name': 'cooperation', 'to': url_for('cooperation')},
         {'name': 'team', 'to': url_for('team')},
-        {'name': 'manual', 'to': 'https://manual.openatlas.eu'},
-        {'name': 'documentation',
-         'to': 'https://redmine.openatlas.eu/projects/uni/wiki'}]
+        {'name': 'events', 'to': url_for('events')}]
 
-    external_links = [
-        {'name': 'Demo', 'to': 'https://demo.openatlas.eu'},
-        {'name': 'Development Demo', 'to': 'https://demo-dev.openatlas.eu'},
+    documentation_links = [
         {'name': 'Features',
-         'to': 'https://manual.openatlas.eu/features.html'},
+        'to': 'https://manual.openatlas.eu/features.html'},
         {'name': 'Manual', 'to': 'https://manual.openatlas.eu'},
-        {'name': 'Documentation',
-         'to': 'https://redmine.openatlas.eu/projects/uni/wiki'},
+        {'name': 'Redmine',
+        'to': 'https://redmine.openatlas.eu/projects/uni/wiki'},
         {'name': 'Model', 'to': 'https://demo.openatlas.eu/overview/model'},
         {'name': 'Code', 'to': 'https://github.com/craws/OpenAtlas'}]
-    return dict(menu=menu, content=content, external_links=external_links)
+
+    demo_links = [
+        {'name': 'Demo', 'to': 'https://demo.openatlas.eu'},
+        {'name': 'Development Demo', 'to': 'https://demo-dev.openatlas.eu'},
+    ]
+
+    return dict(
+        menu=menu,
+        content=content,
+        documentation_links=documentation_links,
+        demo_links=demo_links)
 
 
 @app.template_filter()
