eb1219d2e4daf4d1de12d78c1648a263a0621814
Alexander Watzinger <alexander.watzinger@craws.net>
2026-04-15 16:39:41 +0200

    Update news

--- openatlas_website/data/news.py
+++ openatlas_website/data/news.py
@@ -8,6 +8,14 @@ def release_text(news_id: int) -> str:
 
 news_ = {
     'News': {
+        '2026-04-15': {
+            'title':
+                'OpenAtlas version 9.2.0 and a new cooperation with WHEIM',
+            'text': release_text(53) +
+                    '<br>Also, we are happy to announce a new cooperation '
+                    'with WHEIM.<br>'
+                    'You can find more details at the '
+                    '<a href="/projects">projects</a> page.'},
         '2026-02-06': {
             'title':
                 'OpenAtlas version 9.1.0, OpenAtlas Discovery 1.0.0',
